b58e41ec640454f51bdbbe97b4be5ddaf48ca4ba
lorenzop <lorenzo@poixson.com>
2017-12-15 08:53:36 -0500

    added empty AboutWindow class

---
+++ src/com/poixson/nettet/tool/gui/about/AboutWindow.java
@@ -0,0 +1,11 @@
+package com.poixson.nettet.tool.gui.about;
+
+import com.poixson.app.gui.xWindow;
+
+
+public class AboutWindow extends xWindow {
+	private static final long serialVersionUID = 1L;
+
+
+
+}
